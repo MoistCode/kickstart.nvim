616269a8a11a9ba99f5cb297f585d51ff1f56cbf
Tommy Pham <tommy.pham@hungryroot.com>
2026-01-13 21:46:29 -0500

    fuzzy search and easily open/close diffview

--- init.lua
+++ init.lua
@@ -518,12 +518,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          mappings = {
+            i = { ['<c-space>'] = 'to_fuzzy_refine' },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
--- lua/kickstart/plugins/diffview.lua
+++ lua/kickstart/plugins/diffview.lua
@@ -1,3 +1,7 @@
 return {
   'sindrets/diffview.nvim',
+  keys = {
+    { '<leader>gd', '<cmd>DiffviewOpen<cr>', desc = '[G]it [D]iff open' },
+    { '<leader>gD', '<cmd>DiffviewClose<cr>', desc = '[G]it [D]iff close' },
+  },
 }
